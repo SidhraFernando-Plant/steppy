c99f0a45ed31e0a90412c8b6057daa55d58d3597
“SidhraFernando-Plant <s3873756@student.rmit.edu.au>
2023-08-22 23:32:47 +1000

    Create clockface UI

--- src/presentation/view/Homepage/Timer.tsx
+++ src/presentation/view/Homepage/Timer.tsx
@@ -1,1 +1,20 @@
-export {}
+import React from 'react'
+import './Timer.css'
+
+type TimerProps = {
+
+}
+
+export default function Timer({ }: TimerProps) {
+  return (
+    <div className='timer-container'>
+        <div className='clock-face'>
+            <div>8</div>
+            <div>8</div>
+            <span>:</span>
+            <div>8</div>
+            <div>8</div>
+        </div>
+    </div>
+  )
+}
